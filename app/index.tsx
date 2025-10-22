9eb0eda25b41cbe921cb6af25e58d155f09462b0
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-10-22 05:16:28 -0400

    updated setup

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,11 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
     </View>
   );
 }
